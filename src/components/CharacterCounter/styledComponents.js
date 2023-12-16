bcabc505d8fb33ca88aec3068e266733707574a7
shanmukheswarikondeti <eswarikondeti@gmail.com>
2023-12-16 06:29:34 +0000

    character counter

---
+++ src/components/CharacterCounter/styledComponents.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+`
+export const YellowBgContainer = styled.div`
+  background-color: #ffc533;
+  height: 100vh;
+  width: 50%;
+  padding: 15px;
+`
+export const CharacterInfoContainer = styled.div`
+  background-color: #ffbf1f;
+  border-radius: 15px;
+  padding: 25px;
+`
+export const Heading = styled.h1`
+  font-size: 35px;
+  font-weight: 600;
+  font-family: 'Roboto';
+  color: #0f172a;
+`
+export const UserInputsList = styled.ul`
+  margin-top: 50px;
+`
+export const Image = styled.img`
+  height: 400px;
+  width: 100%;
+  padding: 50px;
+  margin-top: 60px;
+`
+export const CharacterCountContainer = styled.div`
+  background-color: #0f172a;
+  height: 100vh;
+  width: 50%;
+  padding: 20px;
+`
+export const CharacterHeading = styled.h1`
+  font-size: 50px;
+  font-weight: 600;
+  text-align: center;
+  color: #ffbf1f;
+  margin-bottom: 50px;
+`
+export const InputContainer = styled.form`
+  display: flex;
+  justify-content: center;
+`
+export const Input = styled.input`
+  font-size: 18px;
+  font-family: 'Roboto';
+  font-weight: normal;
+  height: 50px;
+  width: 350px;
+  color: #1e293b;
+  background-color: #ffffff;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-width: 0px;
+  border-style: solid;
+  padding: 20px;
+`
+export const AddButton = styled.button`
+  font-size: 18px;
+  margin-left: 10px;
+  font-weight: 600;
+  height: 50px;
+  color: #1e293b;
+  background-color: #ffbf1;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-width: 0px;
+  border-style: solid;
+  padding: 10px 20px 10px 20px;
+`
